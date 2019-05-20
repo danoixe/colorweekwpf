6e29b1ac9b6caf43cc6cbd37a05d6fd2379d5f0a
danoixe <33001136+danoixe@users.noreply.github.com>
2019-05-20 15:13:45 +0200

    Add files via upload

---
+++ ColorWeekApp/Calendar.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ColorWeek
+{
+    public class Calendar : Control
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public ObservableCollection<Day> Days { get; set; }
+
+        public Calendar()
+        {
+        }
+
+        
+
+        public void CreateCalendar(DateTime targetDate) //tworzymy dany miesiac danego roku
+            {
+                //Days.Clear();
+
+                //we check which day of week is first day of month
+                //and so what date is monday
+                DateTime date = new DateTime(targetDate.Year, targetDate.Month, 1);
+                int dayNumber = ((int)date.DayOfWeek + 6) % 7;
+                if (dayNumber != 0) date = date.AddDays(-dayNumber); 
+
+                //6 weeks and 7 days
+                
+            }
+        
+    }
+}
---
+++ ColorWeekApp/Day.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ColorWeek
+{
+    public class Day : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        
+        private bool isToday;
+
+        public bool IsToday
+        {
+            get { return isToday; }
+            set
+            {
+                isToday = value;
+                if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs("IsToday"));
+            }
+        }
+
+        
+        
+    }
+}
